4bbd70fea88ca6518d2037b1871abad302331d83
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-01-17 20:38:58 +0100

    fixed last errors

--- app/main.py
+++ app/main.py
@@ -12,10 +12,7 @@ class BaseRobot:
     ) -> None:
         self.name = name
         self.weight = weight
-        if isinstance(self, FlyingRobot):
-            self.coords = coords or [0, 0, 0]
-        else:
-            self.coords = coords or [0, 0]
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -40,7 +37,8 @@ class FlyingRobot(BaseRobot):
             weight: int,
             coords: list | None = None
     ) -> None:
-        super().__init__(name, weight, coords)
+        super().__init__(name, weight)
+        self.coords = coords or [0, 0, 0]
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -60,9 +58,12 @@ class DeliveryDrone(FlyingRobot):
     ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
-        self.current_load = current_load
+        if current_load is None:
+            self.current_load = current_load
+        else:
+            self.hook_load(current_load)
 
-    def hook_load(self, cargo: Cargo | None) -> None:
+    def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
 
